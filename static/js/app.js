a4bc67969915a92dc76ed0d48560c467e48d94e8
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2019-02-11 18:07:01 -0500

    _

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,10 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tableColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes" ,"comments"];
+
+function create_table(data, columns) {
+    var table = d3.select('#table-append').append('table').attr("class", "table table-bordered table-hover");
+    var tableHead = table.append('thead');
+    var tableBody = table.append('tbody');
 

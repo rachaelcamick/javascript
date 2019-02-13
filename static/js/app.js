f9b7df0d9450021cba55a3303f8dffda0f8f5c14
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2019-02-12 23:28:36 -0500

    _

--- static/js/app.js
+++ static/js/app.js
@@ -4,11 +4,11 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // use d3 to append one table row for each object
-data.forEach(function(sightings) {
-    console.log(sightings);
+tableData.forEach(function(sighting) {
+    console.log(sighting);
     var row = tbody.append("tr");
     // use d3 to append 1 cell per weather report value
-    Object.defineProperties(sightings).forEach(function([key, value]) {
+    Object.entries(sighting).forEach(function([key, value]) {
         console.log(key, value);
         var cell = tbody.append("td");
         //add text
